9900e49960ab5ca0ce111fa912d19b733ed06be7
Anna Kolganova <krk@feip.co>
2023-04-27 11:10:43 +1000

    change garbage view, add alert points

--- Assets/Scripts/UI/Points/Points.cs
+++ Assets/Scripts/UI/Points/Points.cs
@@ -5,10 +5,13 @@ public class Points : MonoBehaviour
 {
     [SerializeField] private RectTransform rectTransform;
     [SerializeField] private Text text;
+    [SerializeField] private Animator animator;
     [SerializeField] private PointVariation incObject;
     [SerializeField] private PointVariation decObject;
+    
+    [SerializeField] private int alertValue;
 
-    public int value;
+    private int value;
 
     public void AnimatedChange(int newValue, int variation)
     {
@@ -37,5 +40,12 @@ public class Points : MonoBehaviour
     private void UpdateText()
     {
         text.text = value.ToString();
+
+        if (value <= alertValue)
+        {
+            animator.SetBool("alert", true);
+        } else {
+            animator.SetBool("alert", false);
+        }
     }
 }
